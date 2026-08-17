e1c840682994d2d4a4abf9db8b2cdc0f50044aad
GPT Slave <unknown.entiti911@gmail.com>
2026-08-18 00:17:30 +0100

    remove unused desktop reexports

--- src/desktop/mod.rs
+++ src/desktop/mod.rs
@@ -1,9 +1,7 @@
 mod classify;
 mod model;
 
-pub use model::{
-    ApplicationInfo, DesktopSnapshot, DisplayInfo, IgnoredCandidate, Rect, WindowInfo,
-};
+pub use model::DesktopSnapshot;
 
 #[cfg(windows)]
 mod windows;
@@ -43,7 +41,9 @@ fn annotate_direct_application_owners(snapshot: &mut DesktopSnapshot) {
 
 #[cfg(test)]
 mod tests {
-    use super::model::{ApplicationClassification, WindowState};
+    use super::model::{
+        ApplicationClassification, ApplicationInfo, IgnoredCandidate, Rect, WindowInfo, WindowState,
+    };
     use super::*;
 
     fn application(pid: u32, name: &str, windows: Vec<WindowInfo>) -> ApplicationInfo {
